23be7a9dda34d305d2868498be231211aeaa8414
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-22 09:37:53 +0100

    chore: updated next.config.mjs to remove invalid options

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  future: { webpack5: true },
   webpack(config) {
     config.resolve.fallback = { ...config.resolve.fallback, fs: false };
     return config;
